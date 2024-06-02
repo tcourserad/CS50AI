8da6c72f177fe650aca31cab6ff3807dc859bb4d
tcourserad <tcourserad@users.noreply.github.com>
2024-06-02 12:43:18 +0200

    somehow working :)

--- 1 Knowledge/Project 1/knights/puzzle.py
+++ 1 Knowledge/Project 1/knights/puzzle.py
@@ -12,21 +12,48 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+  
+    Or(AKnight, AKnave),
+    Not(And(AKnight, AKnave)), # not both
+    
+    # AKnight Truth, AKnave Lie
+    Implication(AKnight, And(AKnight, AKnave)),
+    Implication(AKnave, Not(And(AKnight, AKnave)))
+
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    Or(AKnight, AKnave),
+    Not(And(AKnight, AKnave)), # not both
+    Or(BKnight, BKnave),
+    Not(And(BKnight, BKnave)), # not both
+    
+    # AKnight Truth, AKnave Lie
+    Implication(AKnight, And(AKnave, BKnave)),
+    Implication(AKnave, Not(And(AKnave, BKnave))),
+    
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    Or(AKnight, AKnave),
+    Not(And(AKnight, AKnave)), # not both
+    Or(BKnight, BKnave),
+    Not(And(BKnight, BKnave)), # not both
+
+    # AKnight Truth, AKnave Lie
+    Implication(AKnight, Or(And(AKnight, BKnight), And(AKnave, BKnave))),
+    Implication(AKnave, Not(Or(And(AKnight, BKnight), And(AKnave, BKnave)))),
+    # BKnight Truth, BKnave Lie
+    Implication(BKnight, Or(And(AKnight, BKnave), And(AKnave, BKnight))),
+    Implication(BKnave, Not(Or(And(AKnight, BKnave), And(AKnave, BKnight)))),
+               
+    
 )
 
 # Puzzle 3
@@ -35,7 +62,33 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+    
+    Or(AKnight, AKnave),
+    Not(And(AKnight, AKnave)), # not both
+    Or(BKnight, BKnave),
+    Not(And(BKnight, BKnave)), # not both
+    Or(CKnight, CKnave),
+    Not(And(CKnight, CKnave)), # not both
+
+    # AKnight Truth, AKnave Lie
+    # A says either "I am a knight." or "I am a knave.", but you don't know which.
+    Implication(AKnight, Or(AKnight, AKnave)),
+    Implication(AKnave, Not(Or(AKnight, AKnave))),
+    
+    # BKnight Truth, BKnave Lie
+    # B says "A said 'I am a knave'."
+    
+    
+    # B says "C is a knave."
+    Implication(BKnight, CKnave),
+    Implication(BKnave, CKnight),
+    
+    # CKnight Truth, CKnave Lie
+    # C says "A is a knight."
+    Implication(CKnight, AKnight),
+    Implication(CKnave, AKnave),
+    
+
 )
 
 
