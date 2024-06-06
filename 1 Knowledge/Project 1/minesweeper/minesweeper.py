4d74d21994fe6ce614223bfe41f5f68b17145a58
tdurchde <thomas.durchdewald@t-online.de>
2024-06-06 09:41:58 +0200

    pause

--- 1 Knowledge/Project 1/minesweeper/minesweeper.py
+++ 1 Knowledge/Project 1/minesweeper/minesweeper.py
@@ -105,12 +105,17 @@ class Sentence():
         """
         Returns the set of all cells in self.cells known to be mines.
         """
-        raise NotImplementedError
+        if len(self.cells) == self.count:
+            return self.cells
+
+        return None
 
     def known_safes(self):
         """
         Returns the set of all cells in self.cells known to be safe.
         """
+        
+
         raise NotImplementedError
 
     def mark_mine(self, cell):
